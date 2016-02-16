035fed23d4a99a6c1b33fcf777d1b4785120650d
aslmbc13 <asl_mbc_13@hotmail.com>
2016-02-16 18:31:52 +0000

    Bugfixes

--- JARVIS Pi/src/configuration/language/TextSystem.java
+++ JARVIS Pi/src/configuration/language/TextSystem.java
@@ -20,19 +20,21 @@ public class TextSystem {
 	private final static String defaultUserInputFile = "text-system-in.txt";
 	private final static String defaultUserOutputFile = "text-system-out.txt";
 	
-	private static ArrayList<String> greets = new ArrayList<String>(Arrays.asList("good morning jarvis", "good afternoon jarvis", "good evening jarvis", "hello jarvis", "hey jarvis", "hi jarvis", "jarvis" ));
-	private static ArrayList<String> farewell = new ArrayList<String>(Arrays.asList("farewell jarvis", "goodbye jarvis", "bye jarvis", "terminate jarvis", "jarvis terminate" ));
-	private static ArrayList<String> weather_current = new ArrayList<String>(Arrays.asList( "what's today's weather", "what's the weather like", "how's the weather", "is it raining", "is it sunny" ));
 	private final static String greets_trigger = "/*GREET*/";
 	private final static String farewell_trigger = "/*FAREWELL*/";
 	private final static String weather_current_trigger = "/*WEATHER_CURRENT*/";
 	private final static String input_triggers[] = {greets_trigger, farewell_trigger, weather_current_trigger};
-	private final static Object input_lists[] = { greets, farewell, weather_current };
+	private final static Object input_lists[] = { new ArrayList<String>(Arrays.asList("good morning jarvis", "good afternoon jarvis", "good evening jarvis", "hello jarvis", "hey jarvis", "hi jarvis", "jarvis" ))
+		, new ArrayList<String>(Arrays.asList("farewell jarvis", "goodbye jarvis", "bye jarvis", "terminate jarvis", "jarvis terminate" ))
+		, new ArrayList<String>(Arrays.asList( "what's today's weather", "what's the weather like", "how's the weather", "is it raining", "is it sunny" )) };
+	private final static int greets_index = 0;
+	private final static int farewell_index = 1;
+	private final static int weather_current_index = 2;
 	
-	private static ArrayList<String> already_here = new ArrayList<String>(Arrays.asList("i'm here USER", "i can hear you USER", "how should i serve you USER", "what can i do for you USER", "how may i be of service USER"));
 	private final static String already_here_trigger = "/*ALREADY_HERE*/";
 	private final static String output_triggers[] = {already_here_trigger};
-	private final static Object output_lists[] = {already_here};
+	private final static Object output_lists[] = {new ArrayList<String>(Arrays.asList("i'm here USER", "i can hear you USER", "how should i serve you USER", "what can i do for you USER", "how may i be of service USER"))};
+	private final static int already_here_index = 0;
 
 	private final static String greetReplies[] = { "good morning ", "good afternoon ", "good evening "};
 	private static enum greetRepliesType { MORNING, AFTERNOON, EVENING };
@@ -103,25 +105,29 @@ public class TextSystem {
 		return farewellReply;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public static final ArrayList<String> getGreets() {
-		return greets;
+		return (ArrayList<String>) input_lists[greets_index];
 	}
 	
+	@SuppressWarnings("unchecked")
 	public static final ArrayList<String> getFarewell() {
-		return farewell;
+		return (ArrayList<String>) input_lists[farewell_index];
 	}
 	
+	@SuppressWarnings("unchecked")
 	public static final ArrayList<String> getAlreadyHere() {
-		return already_here;
+		return (ArrayList<String>) output_lists[already_here_index];
 	}
 	
+	@SuppressWarnings("unchecked")
 	public static final String getRandomAlreadyHere() {
 		Random rn = new Random();
-		int index = rn.nextInt() % already_here.size();
+		int index = rn.nextInt() % ((ArrayList<String>) output_lists[already_here_index]).size();
 		
-		if(already_here.get(index).contains("USER")) {
+		if(((ArrayList<String>)output_lists[already_here_index]).get(index).contains("USER")) {
 			try {
-				return already_here.get(index).replace("USER", Configuration.getInstance().getUserName());
+				return ((ArrayList<String>)output_lists[already_here_index]).get(index).replace("USER", Configuration.getInstance().getUserName());
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
@@ -129,23 +135,27 @@ public class TextSystem {
 			}
 		}
 		
-		return already_here.get(index);
+		return ((ArrayList<String>)output_lists[already_here_index]).get(index);
 	}
 	
+	@SuppressWarnings("unchecked")
 	public static final ArrayList<String> getWeatherCondition() {
-		return weather_current;
+		return (ArrayList<String>) input_lists[weather_current_index];
 	}
 	
+	@SuppressWarnings("unchecked")
 	public static final Boolean isGreet(String message) {
-		return greets.contains(message);
+		return ((ArrayList<String>) input_lists[greets_index]).contains(message);
 	}
 
+	@SuppressWarnings("unchecked")
 	public static final Boolean isFarewell(String message) {
-		return farewell.contains(message);
+		return ((ArrayList<String>) input_lists[farewell_index]).contains(message);
 	}
 
+	@SuppressWarnings("unchecked")
 	public static boolean isWeatherCondition(String message) {
-		return weather_current.contains(message);
+		return ((ArrayList<String>) input_lists[weather_current_index]).contains(message);
 	}
 
 	public static String weatherConditionMessage(String city, String weather, double temperatureCelsius) {
